eb09a597eedd089b66a9bc7d28dbe46d71c38f16
Ryan Robinson <ryan.b.robinson@gmail.com>
2014-07-16 12:20:12 -0700

    Added applicationCtrl.js and updated app.js and index.html to use this top level controller

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,45 +8,60 @@
  *
  * Main module of the application.
  */
-angular
-  .module('voyagerDesktopApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch',
-    'leaflet-directive',
-    'geolocation'
+ angular
+ .module('voyagerDesktopApp', [
+  'ngAnimate',
+  'ngCookies',
+  'ngResource',
+  'ngRoute',
+  'ngSanitize',
+  'ngTouch',
+  'leaflet-directive',
+  'geolocation'
   ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/expeditionList.html',
-        controller: 'listCtrl'
-      })
-      .when('/expeditions', {
-        templateUrl: 'views/expeditionList.html',
-        controller: 'listCtrl'
-      })
-      .when('/expeditions/new',{
-        templateUrl: 'views/expeditionEdit.html',
-        controller: 'expeditionEditCtrl'
-      })
-      .when('/expeditions/:id',{
-        templateUrl: 'views/expeditionShow.html',
-        controller: 'expeditionCtrl'
-      })
-      .when('/expeditions/:id/edit',{
-        templateUrl: 'views/expeditionEdit.html',
-        controller: 'expeditionEditCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+ .config(function ($routeProvider) {
+  $routeProvider
+  .when('/', {
+    templateUrl: 'views/login.html',
+    controller: 'loginCtrl'
   })
-  .config(['$httpProvider', function($httpProvider) {
-        $httpProvider.defaults.useXDomain = true;
-        delete $httpProvider.defaults.headers.common['X-Requested-With'];
-    }
-]);
+  .when('/expeditions', {
+    templateUrl: 'views/expeditionList.html',
+    controller: 'listCtrl'
+  })
+  .when('/expeditions/new',{
+    templateUrl: 'views/expeditionEdit.html',
+    controller: 'expeditionEditCtrl',
+    isLogin: true
+  })
+  .when('/expeditions/:id',{
+    templateUrl: 'views/expeditionShow.html',
+    controller: 'expeditionCtrl'
+  })
+  .when('/expeditions/:id/edit',{
+    templateUrl: 'views/expeditionEdit.html',
+    controller: 'expeditionEditCtrl'
+  })
+  .otherwise({
+    redirectTo: '/'
+  });
+})
+ .config(['$httpProvider', function($httpProvider) {
+  $httpProvider.defaults.useXDomain = true;
+  delete $httpProvider.defaults.headers.common['X-Requested-With'];
+}])
+ .constant('AUTH_EVENTS', {
+  loginSuccess: 'auth-login-success',
+  loginFailed: 'auth-login-failed',
+  logoutSuccess: 'auth-logout-success',
+  sessionTimeout: 'auth-session-timeout',
+  notAuthenticated: 'auth-not-authenticated',
+  notAuthorized: 'auth-not-authorized'
+})
+ .constant('USER_ROLES', {
+  all: '*',
+  admin: 'admin',
+  editor: 'editor',
+  guest: 'guest'
+})
+ .constant("API_ENDPOINT", "http://localhost:8128")
---
+++ app/scripts/controllers/applicationCtrl.js
@@ -0,0 +1,30 @@
+'user-strict'
+
+angular.module('voyagerDesktopApp')
+.controller('ApplicationCtrl', function ($scope, $rootScope, $location, USER_ROLES, AuthService) {
+  $scope.currentUser = null;
+  $scope.userRoles = USER_ROLES;
+  $scope.isAuthorized = AuthService.isAuthorized;
+ 
+  $scope.setCurrentUser = function (user) {
+    $scope.currentUser = user;
+  };
+
+  $rootScope.$on('auth-login-success', function(evt, cur, prev) {
+  	var returnTo = $rootScope.savedLocation || "/expeditions/";
+  	$location.path(returnTo)
+  });
+
+
+  // Redirects users to login form for login-restricted urls
+  $rootScope.$on('$routeChangeStart', function (event, next) {
+  	var userAuthenticated = $scope.currentUser;
+
+  	if (!userAuthenticated && next.isLogin) {
+  		/* You can save the user's location to take him back to the same page after he has logged-in */
+  		$rootScope.savedLocation = $location.url();
+
+  		$location.path('/');
+  	}
+  });
+})
