3f4f6be6a548c83ace748c8dac79b50eefe0b3f4
Ryan Robinson <ryan.b.robinson@gmail.com>
2014-07-20 23:52:52 -0700

    Updated expeditionEditCtrl.js to show error/success messages on save and update; could be refactored

--- app/scripts/controllers/expeditionEditCtrl.js
+++ app/scripts/controllers/expeditionEditCtrl.js
@@ -8,7 +8,8 @@
  * Controller of the voyagerDesktopApp
  */
 angular.module('voyagerDesktopApp')
-  .controller('expeditionEditCtrl', ['$scope','$routeParams','ExpeditionFactory',function ($scope, $routeParams, exp) {
+  .controller('expeditionEditCtrl', ['$scope','$routeParams','ExpeditionFactory','MessageService', 
+    function ($scope, $routeParams, exp, MessageService) {
 
 
   	var expedition = new exp({
@@ -23,22 +24,77 @@ angular.module('voyagerDesktopApp')
         user : {},
   	});
 
+    var saveSuccessHandler = function(data){
+      expedition._id = data._id;
 
-  	console.log(expedition)
+      var successMessage = {
+        severity: "success",
+        title: "Success!",
+        text: "Expedition created!"
+      }
+
+      MessageService.addMessage(successMessage);
+
+      setTimeout(function(){
+        MessageService.removeMessage(successMessage)
+      }, 5000);
+
+    }
+
+    var saveErrorHandler = function(data){
+
+      var errorMessage = {
+        severity: "danger",
+        title: "Oh Snap!",
+        text: data.error || "There was an error creating this expedition!"
+      }
+
+      MessageService.addMessage(errorMessage);
+
+      setTimeout(function(){
+        MessageService.removeMessage(errorMessage)
+      }, 5000);
+    }
+
+    var updateSuccessHandler = function(data){
+
+      var successMessage = {
+        severity: "success",
+        title: "Success!",
+        text: "Expedition updated!"
+      }
+
+      MessageService.addMessage(successMessage);
+
+      setTimeout(function(){
+        MessageService.removeMessage(successMessage)
+      }, 5000);
+
+    }
+
+    var updateErrorHandler = function(data){
+
+      var errorMessage = {
+        severity: "danger",
+        title: "Oh Snap!",
+        text: data.error || "There was an error updating this expedition!"
+      }
+
+      MessageService.addMessage(errorMessage);
+
+      setTimeout(function(){
+        MessageService.removeMessage(errorMessage)
+      }, 5000);
+    }
 
   	$scope.save = function(){
   		if (!expedition._id){
-	  		expedition.$save(function(data){
-	  			console.log(data)
-	  		})
+	  		expedition.$save(saveSuccessHandler,saveErrorHandler)
   		} else {
-  			expedition.$update(angular.extend({ id: expedition._id }, expedition),function(data){
-  				console.log(data)
-  			})
-  		}
-  	}
+  			expedition.$update(angular.extend({ id: expedition._id }, expedition), updateSuccessHandler,updateErrorHandler);
+      }
+    }
 
   	$scope.expedition = expedition;
 
-    //$scope.expedition : exp.show({ id: $routeParams.id });
   }]);
